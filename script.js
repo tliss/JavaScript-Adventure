c75fb0f5ec7662f6cbefbd7aab11f2c4bbef3b5e
Taylor Liss <tayloraliss@gmail.com>
2017-01-24 14:06:46 -0500

    Add stat panel

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ function loadStart() {
     'use strict';
     var name = document.getElementById("nameBox").value,
         adventurersName = document.getElementById('adventurersName');
+    adventurersName.innerHTML = name;
     document.getElementById("name").style.display = "none";
     document.getElementById("initialSetup").style.display = "block";
-    adventurersName.innerHTML = name;
+    
 }
 
 function mine() {
