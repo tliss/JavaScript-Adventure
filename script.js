d998229a9463ff80fe3232dcf862e2f2ad988e3f
Taylor Liss <tayloraliss@gmail.com>
2017-01-24 11:15:59 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,43 @@
+/*jslint devel: true */
+
+function start() {
+    'use strict';
+    document.getElementById("startBtn").style.display = "none";
+	document.getElementById("name").style.display = "block";
+}
+
+function loadStart() {
+    'use strict';
+    document.getElementById("name").style.display = "none";
+    document.getElementById("initialSetup").style.display = "block";
+}
+
+function mine() {
+    'use strict';
+    document.getElementById("logWindow").innerHTML = "You mine some ore.";
+    
+    var x = document.getElementById("inventory"),
+        option = document.createElement("option");
+    option.text = "ore";
+    x.add(option);
+}
+
+function rest() {
+    'use strict';
+    document.getElementById("logWindow").innerHTML = "You got some rest.";
+    
+    var x = document.getElementById("inventory"),
+        option = document.createElement("option");
+    option.text = "rest";
+    x.add(option);
+}
+
+function select(value) {
+    'use strict';
+    var objContent = document.getElementById('detailsWindow');
+	if (value === "ore") {
+        objContent.innerHTML = "It's ore!";
+    } else {
+        objContent.innerHTML = "I dunno.";
+    }
+}
